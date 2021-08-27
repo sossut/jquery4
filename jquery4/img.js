3bb7f6146e09b08cfa0a764197d5d31ed35324ee
Mikael Alakari <mikaeala@k-d557-mac-17.pk.amk.root>
2021-08-27 12:13:29 +0300

    first commit

---
+++ jquery4/img.js
@@ -0,0 +1,20 @@
+$('button').click(function() {
+    $('img').show();
+    $('ul').html('');
+    $('#advice').text('select your favourite colour among the colours ');
+    $('#sel').text('your favourite coloours in order ');
+    
+    $('img').each(function() {
+        $(this).attr('src', $(this).data('color') + '.jpg');
+    });
+});
+
+$('img').each(function() {
+    $(this).attr('src', $(this).data('color') + '.jpg');
+});
+
+$('img').click(function(){
+    $('ul').append('<li>' + $(this).data('color') + '</li>');
+    $(this).hide();
+});
+
